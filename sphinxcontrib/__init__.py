dcfa464fc3b2012850a798d2ac5ef0e3fda6fe85
Thomas Van Doren <thomas.vandoren@gmail.com>
2014-12-10 00:05:27 -0800

    meh, try to different whitespace

--- sphinxcontrib/__init__.py
+++ sphinxcontrib/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""sphinxcontrib
-~~~~~~~~~~~~~
+"""
+    sphinxcontrib
+    ~~~~~~~~~~~~~
 
-This package is a namespace package that contains all extensions distributed in
-the ``sphinx-contrib`` distribution.
+    This package is a namespace package that contains all extensions
+    distributed in the ``sphinx-contrib`` distribution.
 """
 
 __import__('pkg_resources').declare_namespace(__name__)
