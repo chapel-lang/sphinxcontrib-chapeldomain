edb4f111a608aaba9807c5e828e41b2a0e391cf1
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-01-25 15:11:54 -0800

    Start on some tests..

--- test/test_chapeldomain.py
+++ test/test_chapeldomain.py
@@ -7,7 +7,8 @@ from __future__ import print_function, unicode_literals
 import mock
 import unittest
 
-from sphinxcontrib.chapeldomain import ChapelDomain
+from sphinxcontrib.chapeldomain import (
+    ChapelDomain, chpl_sig_pattern, chpl_attr_sig_pattern)
 
 
 class ChapelDomainTests(unittest.TestCase):
@@ -18,6 +19,41 @@ class ChapelDomainTests(unittest.TestCase):
         env = mock.Mock()
         env.domaindata = {'name': 'chapel'}
         self.assertIsNotNone(ChapelDomain(env))
+
+
+class PatternTestCase(unittest.TestCase):
+    """Helper methods for regex pattern tests."""
+
+    pass
+
+
+class SigPatternTests(PatternTestCase):
+    """Verify chpl_sig_pattern regex."""
+
+    def test_todo(self):
+        self.fail('no')
+
+
+class AttrSigPatternTests(PatternTestCase):
+    """Verify chpl_attr_sig_pattern regex."""
+
+    def test_does_not_match(self):
+        """Verify various signatures that should not match."""
+        sig = '...'
+        match = chpl_attr_sig_pattern.match(sig)
+        self.assertIsNone(match)
+
+    def test_simple_label(self):
+        """Verify various symbols match pattern."""
+        sig = 'myVar'
+        match = chpl_attr_sig_pattern.match(sig)
+        self.assertIsNotNone(match)
+
+        func_prefix, name_prefix, name, retann = match.groups()
+        self.assertEqual('', func_prefix)
+        self.assertEqual(None, name_prefix)
+        self.assertEqual(sig, name)
+        self.assertEqual(None, retann)
 
 
 if __name__ == '__main__':
