425bf2e56c0e551ea9da3944d4bc3cd767d21e30
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-01-25 22:45:08 -0800

    Add some more unittests.

--- test/test_chapeldomain.py
+++ test/test_chapeldomain.py
@@ -24,36 +24,95 @@ class ChapelDomainTests(unittest.TestCase):
 class PatternTestCase(unittest.TestCase):
     """Helper methods for regex pattern tests."""
 
-    pass
+    def check_is_not_match(self, sig):
+        """Verify sig does not match attr pattern."""
+        match = self.pattern.match(sig)
+        self.assertIsNone(match)
 
 
 class SigPatternTests(PatternTestCase):
     """Verify chpl_sig_pattern regex."""
 
-    def test_todo(self):
-        self.fail('no')
+    pattern = chpl_sig_pattern
+
+    def check_sig(self, sig, func_prefix, name_prefix, name, arglist, retann):
+        """Verify signature results in appropriate matches."""
+        match = self.pattern.match(sig)
+        self.assertIsNotNone(match)
+
+        (actual_func_prefix, actual_name_prefix, actual_name, actual_arglist, actual_retann) = match.groups()
+        self.assertEqual(func_prefix, actual_func_prefix)
+        self.assertEqual(name_prefix, actual_name_prefix)
+        self.assertEqual(name, actual_name)
+        self.assertEqual(arglist, actual_arglist)
+        self.assertEqual(retann, actual_retann)
+
+    def test_does_not_match(self):
+        """Verify various signatures that should not match."""
+        test_cases = [
+            '...',
+            '.',
+            '-',
+            '---',
+            ':::',
+            '@',
+            '#',
+        ]
+        for sig in test_cases:
+            self.check_is_not_match(sig)
 
 
 class AttrSigPatternTests(PatternTestCase):
     """Verify chpl_attr_sig_pattern regex."""
 
+    pattern = chpl_attr_sig_pattern
+
+    def check_sig(self, sig, func_prefix, name_prefix, name, retann):
+        """Verify signature results in appropriate matches."""
+        match = self.pattern.match(sig)
+        self.assertIsNotNone(match)
+
+        (actual_func_prefix, actual_name_prefix, actual_name, actual_retann) = match.groups()
+        self.assertEqual(func_prefix, actual_func_prefix)
+        self.assertEqual(name_prefix, actual_name_prefix)
+        self.assertEqual(name, actual_name)
+        self.assertEqual(retann, actual_retann)
+
     def test_does_not_match(self):
         """Verify various signatures that should not match."""
-        sig = '...'
-        match = chpl_attr_sig_pattern.match(sig)
-        self.assertIsNone(match)
+        test_cases = [
+            '...',
+            '.',
+            '-',
+            '----',
+            ':::',
+            '@',
+            '#',
+        ]
+        for sig in test_cases:
+            self.check_is_not_match(sig)
 
     def test_simple_label(self):
         """Verify various symbols match pattern."""
-        sig = 'myVar'
-        match = chpl_attr_sig_pattern.match(sig)
-        self.assertIsNotNone(match)
+        test_cases = [
+            'x',
+            'myVar',
+            'l_l',
+            '123',
+            '1',
+        ]
+        for sig in test_cases:
+            self.check_sig(sig, '', None, sig, None)
 
-        func_prefix, name_prefix, name, retann = match.groups()
-        self.assertEqual('', func_prefix)
-        self.assertEqual(None, name_prefix)
-        self.assertEqual(sig, name)
-        self.assertEqual(None, retann)
+    def test_with_class_names(self):
+        """Verify symbols with class names match pattern."""
+        test_cases = [
+            ('my.var', 'my.', 'var'),
+            ('1.1', '1.', '1'),
+            ('BigNum.fromInt', 'BigNum.', 'fromInt'),
+        ]
+        for sig, class_name, attr in test_cases:
+            self.check_sig(sig, '', class_name, attr, None)
 
 
 if __name__ == '__main__':
