5dbe663fb6cfeaa1e65015b34645fed4c693d10f
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-02-26 11:06:51 -0800

    bump version to 0.0.9

--- sphinxcontrib/chapeldomain.py
+++ sphinxcontrib/chapeldomain.py
@@ -30,7 +30,7 @@ from sphinx.util.docfields import Field, TypedField
 from sphinx.util.nodes import make_refnode
 
 
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 
 
 # regex for parsing proc, iter, class, record, etc.
