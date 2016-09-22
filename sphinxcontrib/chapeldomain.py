9c2709e154f3030535d2a031233fbbb5e0b9c2a5
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2016-09-22 10:34:49 -0700

    Fix indentation now that the regex finally works.

--- sphinxcontrib/chapeldomain.py
+++ sphinxcontrib/chapeldomain.py
@@ -35,15 +35,15 @@ VERSION = '0.0.13'
 
 # regex for parsing proc, iter, class, record, etc.
 chpl_sig_pattern = re.compile(
-    r"""^ ((?:\w+\s+)*                     # optional: prefixes
-           (?:proc|iter|class|record)\s+   #   must end with keyword
-           (?:type\s+|param\s+)?           # optional: type or param method
+    r"""^ ((?:\w+\s+)*                        # optional: prefixes
+           (?:proc|iter|class|record)\s+      #   must end with keyword
+           (?:type\s+|param\s+)?              # optional: type or param method
           )?
-          ([\w$.]*\.)?                     # class name(s)
-          ([\w\+\-/\*$]+)  \s*             # function or method name
-          (?:\((.*?)\))?                   # optional: arguments
-          (\s+(?:const\s)? \w+|              #   or return intent
-           \s* : \s* [^:]+|                #   or return type
+          ([\w$.]*\.)?                        # class name(s)
+          ([\w\+\-/\*$]+)  \s*                # function or method name
+          (?:\((.*?)\))?                      # optional: arguments
+          (\s+(?:const\s)? \w+|               #   or return intent
+           \s* : \s* [^:]+|                   #   or return type
            \s+(?:const\s)? \w+\s* : \s* [^:]+ #   or return intent and type
           )?
           $""", re.VERBOSE)
